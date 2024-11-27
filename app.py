ecdb1c22f0dc8448062a29324a63d5f29ebe8fd6
San086 <andreas.julien-caraguel@etu.unilasalle.fr>
2024-11-28 00:02:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+st.title('Projet python oiseaux de Marseille !')
+st.write('Lea COQUEREAU \n Guillaume VALENTIN \n Andreas JULIEN-CARAGUEL')
+
 
