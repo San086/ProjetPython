b1b25c19089a9e87b6b05c8d938629b03742a591
San086 <andreas.julien-caraguel@etu.unilasalle.fr>
2024-11-28 00:20:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ import pandas as pd
 import numpy as np
 import math
 
-
 st.title('Projet oiseaux de Marseille !')
 st.text("Lea COQUEREAU\nGuillaume VALENTIN\nAndreas JULIEN-CARAGUEL")
 
 
+fichier = "marseille_biodiversite_oiseaux_parcs.csv"
+data = pd.read_csv(fichier)
+
+df = pd.DataFrame(values, columns=feature_names)
+df
+plot = df['Nom du site']
+
+st.line_chart(plot)
