3a452db27bc12c0628cb9a42190a433acb77e32f
San086 <andreas.julien-caraguel@etu.unilasalle.fr>
2024-11-28 01:15:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,19 +19,19 @@ df
 
 st.header("Distribution des noms des sites", divider=True)
 bar = df["Nom du site"].str.split(" ;").str[0]
-st.line_chart(bar)
+st.scatter_chart(bar)
 
 st.header("Distribution des types", divider=True)
 ty = df["Type"]
-st.line_chart(ty)
+st.scatter_chart(ty)
 
 st.header("Nombre d'espèces observer par parc", divider=True)
 df_grouped = df.groupby("Période d'observation")["Nom du site"].first().reset_index()
-st.line_chart(df_grouped)
+st.scatter_chart(df_grouped)
 
 st.header("Nombre d'observation par espèce à Marseille", divider=True)
 ver = df["Nom vernaculaire"]
-st.line_chart(ver)
+st.scatter_chart(ver)
 
 
 st.header("Tableau du nombre d'espèce", divider=True)
