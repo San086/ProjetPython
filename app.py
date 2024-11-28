6487d7f794543bee91f9c823b2a8f0f7145f4c70
San086 <andreas.julien-caraguel@etu.unilasalle.fr>
2024-11-28 01:28:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
-import plotly.express as px
+import plotly as px
 
 
 st.title('Projet oiseaux :bird: de :blue[Marseille] :sunglasses:')
